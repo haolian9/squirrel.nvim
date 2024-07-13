328428aba6209c97327d73f6bd976de0aae37485
haoliang <haoliang0.1.2@gmail.com>
2024-07-13 10:39:28 +0800

    sync with upstream

--- lua/squirrel/insert_import/facts.lua
+++ lua/squirrel/insert_import/facts.lua
@@ -3,9 +3,9 @@ local M = {}
 do
   ---@type TSNode
   local origin = {}
-  function origin:start() return -1, 0 end
-  function origin:end_() return -1, 0 end
-  function origin:range() return -1, 0, -1, 0 end
+  function origin:start() return 0, 0 end
+  function origin:end_() return 0, 0 end
+  function origin:range() return 0, 0, 0, 0 end
 
   M.origin = origin
 end
